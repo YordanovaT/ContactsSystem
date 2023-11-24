48f70b9fa9f2036cd930c1aacced4098a92485ba
YordanovaT <tanyaiyord@gmail.com>
2023-11-24 18:53:45 +0200

    Created header file for contacts book. Added implementation for its methods.

---
+++ address_book.cpp
@@ -0,0 +1,55 @@
+#include "address_book.h"
+
+AddressBook::AddressBook()
+{
+    contacts_count=0;
+}
+
+void AddressBook::add_contact(Person *person)
+{
+    if(contacts_count>30)
+    {
+        std::cout<<"Sorry, you have reached the max limit for contacts. Try erasing some.\n";
+        return;
+    }
+    persons[contacts_count]=person;
+    contacts_count++;
+}
+
+void AddressBook::erase_contact(int id)
+{
+    int flag=0;
+    if(contacts_count==0)
+    {
+        std::cout<<"Your address book is empty. There's nothing to erase.\n";
+        return;
+    }
+    for(int i=0; i<contacts_count; i++)
+    {
+        if(persons[i]->get_person_id() == id)
+        {
+            persons[i]=persons[contacts_count - 1];
+            flag=1;
+        }
+    }
+    if(flag)
+        contacts_count--;
+    else
+        std::cout<<"Found no person with the given id\n";
+}
+
+void AddressBook::output()
+{
+    for(int i=0; i<contacts_count; i++)
+    {
+        persons[i]->print();
+    }
+}
+
+AddressBook::~AddressBook()
+{
+    for(int i=0; i<contacts_count; i++)
+    {
+        delete persons[i];
+    }
+}
---
+++ address_book.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include "person.h"
+
+
+class AddressBook
+{
+    public:
+        Person *persons[30];
+        int contacts_count;
+
+        AddressBook();
+        ~AddressBook();
+
+        void add_contact(Person *person);
+        void erase_contact(int id);
+        void output();   
+
+};
